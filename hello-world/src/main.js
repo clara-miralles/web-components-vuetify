019be7fa1a1246d37acc9328820a020b642faa96
Clara Miralles <clara.miralles.imperial@gmail.com>
2021-06-23 16:54:07 +0200

    npm install --save vuetify + import Vuetify from "vuetify" + createApp(App).use(Vuetify);

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import vueCustomElement from "vue-custom-element";
+import Vuetify from "vuetify";
 
 createApp(App).use(vueCustomElement);
+createApp(App).use(Vuetify);
 
 createApp(App).mount("#app");
