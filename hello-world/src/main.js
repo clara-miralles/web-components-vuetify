2d005170fa31f8441543aa65f732610c71bd4262
Clara Miralles <clara.miralles.imperial@gmail.com>
2021-06-23 16:43:19 +0200

    import vueCustomElement from "vue-custom-element"; createApp(App).use(vueCustomElement);

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import vueCustomElement from "vue-custom-element";
+
+createApp(App).use(vueCustomElement);
 
 createApp(App).mount("#app");
